cd17d8b34773665b143a0252d1283afa7bfed210
sers1 <sers156@mail.ru>
2023-10-17 15:41:17 +1000

    hw5

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,5 +1,12 @@
 public class Exercise1 {
     public static void main(String[] args) {
-
+        int i1=5;
+        int i2=11;
+        double d1=5.5;
+        double d2=1.3;
+        long l=20L;
+        double result=0;
+        result=i2/d1+d2%i1-l;
+        System.out.println(result);
     }
 }
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,5 +1,8 @@
 public class Exercise2 {
     public static void main(String[] args) {
-
+        int a=5;
+        System.out.println(a-- - --a + ++a +a++ + a);
+        int b=8;
+        System.out.println(++b - b++ + ++b - --b);
     }
 }
